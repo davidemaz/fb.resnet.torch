4371395be3911858576ff9eac6120c30dbb99f82
davidemaz <davidemaz@gmail.com>
2016-06-29 11:23:25 +0200

    add support for different versions of the log file

--- scripts/plotlogs.py
+++ scripts/plotlogs.py
@@ -9,6 +9,22 @@ import csv
 #The first argument of this script is the logfile to plot
 file = sys.argv[1]
 
+version = 2 #default value
+if len(sys.argv) > 2:
+	version = sys.argv[2]
+
+#Set columns indices depending on the version
+if version == 1:
+	top1traincol = 10
+	top5traincol = 12
+	top1testcol = 8
+	top5testcol = 12
+elif version == 2:
+	top1traincol = 11
+	top5traincol = 15
+	top1testcol = 11
+	top5testcol = 15
+
 # Split Train and Test log lines, throw other lines
 reader = csv.reader(open(file, 'rb'))
 lines = list(reader)
@@ -28,12 +44,12 @@ fo = open("tmp_test.txt", "w")
 fo.writelines(line + '\n' for line in testrows)
 
 traindata = pd.read_csv("tmp_train.txt", sep='\s+', skiprows=3, error_bad_lines=False)
-top1errortrain = traindata.ix[:,10].as_matrix()
-top5errortrain = traindata.ix[:,12].as_matrix()
+top1errortrain = traindata.ix[:,top1traincol].as_matrix()
+top5errortrain = traindata.ix[:,top5traincol].as_matrix()
 
 testdata = pd.read_csv("tmp_test.txt", sep='\s+', skiprows=3, error_bad_lines=False)
-top1errortest = testdata.ix[:,8].as_matrix()
-top5errortest = testdata.ix[:,12].as_matrix()
+top1errortest = testdata.ix[:,top1testcol].as_matrix()
+top5errortest = testdata.ix[:,top5testcol].as_matrix()
 
 #Plot
 fig1 = plt.figure(num=None, figsize=(15, 4), dpi=80, facecolor='w', edgecolor='k')
